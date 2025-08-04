87c0e006239b4f66aa155308dfc98ddd7b304605
Pranav Sanotra <pranavsanotra951@gmail.com>
2025-08-04 13:27:57 +0530

    tool functionality

--- agent/agentic_workflow.py
+++ agent/agentic_workflow.py
@@ -9,11 +9,26 @@ from langgraph.prebuilt import ToolNode, tools_condition
 
 
 class Graphbuilder():
-    def __init__(self):
-        self.tools=[
-            #
-        ]
-        pass
+    def __init__(self, model_provider: str  = "groq"):
+        self.model_loaders = Modelloader(model_provider=model_provider)
+        self.llm = self.model_loaders.load_llm()
+        self.tools = []        
+        self.weather_tools = Weatherinfo()
+        self.place_search_tools = Placesearch()
+        self.calculator_tools = Calculator()
+        self.currency_converter_tools = Currencyconversion()
+
+        self.tools.extend([* self.weather_tools.weather_tool_list,
+                           * self.place_search_tools.place_search_tool_list,
+                           * self.calculator_tools.calculator_tool_list,
+                           * self.currency_converter_tools.currency_converter_tool_list])
+        
+        self.llm_with_tools = self.llm.bind_tools(tools=self.tools)
+
+        self.graph = None
+
+
+        self.system_prompt=SYSTEM_PROMPT
 
 
     def agent_function(self, state:MessagesState):
